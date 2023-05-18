f692a57e9f1242c3702a41d9d77c366fd201e861
markalfox <ilya.moiseenko.00@yandex.by>
2023-05-18 22:43:17 +0300

    Did the second task

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         _getAndPrintData();
+        _numericValueOfSymbol();
     }
 
     private static void _getAndPrintData() {
@@ -29,6 +30,15 @@ public class Main {
                 "\nAge: " + age +
                 "\nHeight: " + height +
                 "\nWeight: " + weight);
+    }
+
+    private static void _numericValueOfSymbol() {
+        Scanner scanner = _createScanner();
+
+        System.out.print("Enter the char: ");
+        char value = scanner.next().charAt(0);
+
+        System.out.println(value + ": " + (int)value);
     }
 
     private static Scanner _createScanner() {
