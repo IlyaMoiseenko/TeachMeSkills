bf7c50250a33ab75870dea7d2d14985091ce9dae
markalfox <ilya.moiseenko.00@yandex.by>
2023-05-23 23:03:04 +0300

    Fourth task

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println(_timeOfTheYearSwitch());
         System.out.println(_timeOfTheYearIfElse());
         System.out.println(_evenNumber());
+        System.out.println(_checkTheTemperature());
     }
 
     private static String _timeOfTheYearSwitch() {
@@ -46,6 +47,19 @@ public class Main {
         int num = scanner.nextInt();
 
         return num % 2 == 0;
+    }
+
+    private static String _checkTheTemperature() {
+        Scanner scanner = _createScanner();
+
+        System.out.print("Enter the temperature: ");
+        int temperature = scanner.nextInt();
+
+        if (temperature > -5) return "Тепло";
+        else if (temperature < -5 && temperature > -20) return "Нормально";
+        else if (temperature < -20) return "Холодно";
+
+        return "Please check input data";
     }
 
     private static Scanner _createScanner() {
