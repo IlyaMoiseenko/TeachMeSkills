eeb5cbe0faf73ce52fe57a2983e57105054d08b4
markalfox <ilya.moiseenko.00@yandex.by>
2023-05-23 20:50:49 +0300

    Update template

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
 
-        // print a message without going to the next Line
-        System.out.print ("Welcome to Java.");
-        System.out.print ("This is the next line...");
-
-        // print vs print in
-        System.out.println();
-        System.out.println(122);
     }
 }
