b113ba9dd5538254a0e338bb42a6a6d5c61891c4
IlyaMoiseenko <ilya.moiseenko.00@yandex.by>
2023-05-25 23:02:20 +0300

    First task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        // FIRST TASK
+//        int[] array = WorkWithArray.CreateRandomArray();
+//        System.out.println(Arrays.toString(array));
+//        System.out.println(_findNumber(array, 2));
+    }
 
+    private static boolean _findNumber(int[] numbers, int searchNumber) {
+        for (int number : numbers) {
+            if (number == searchNumber) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
