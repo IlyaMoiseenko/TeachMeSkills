b29667ba000c38eaf4ef2bc86da678f0820d1752
IlyaMoiseenko <ilya.moiseenko.00@yandex.by>
2023-05-26 12:27:44 +0300

    Second task

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,6 +7,15 @@ public class Main {
 //        int[] array = WorkWithArray.CreateRandomArray();
 //        System.out.println(Arrays.toString(array));
 //        System.out.println(_findNumber(array, 2));
+
+
+
+
+//        // SECOND TASK
+//        int[] array = WorkWithArray.CreateArrayFromConsole();
+//        System.out.print("Enter value to delete: ");
+//        array = _removeElement(array, _createScanner().nextInt());
+//        System.out.println(Arrays.toString(array));
     }
 
     private static boolean _findNumber(int[] numbers, int searchNumber) {
@@ -16,5 +26,22 @@ public class Main {
         }
 
         return false;
+    }
+
+    private static int[] _removeElement(int[] nums, int value) {
+        int offset = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == value)
+                offset++;
+            else
+                nums[i - offset] = nums[i];
+        }
+
+        return Arrays.copyOf(nums, nums.length - offset);
+    }
+
+    private static Scanner _createScanner() {
+        return new Scanner((System.in));
     }
 }
