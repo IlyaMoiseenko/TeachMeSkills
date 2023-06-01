e598963684601c40b5ea1bcbcd67a2b612e503a5
IlyaMoiseenko <ilya.moiseenko.00@yandex.by>
2023-06-01 20:27:14 +0300

    Created a file for working with matrices

---
+++ src/WorkWithMatrix.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class WorkWithMatrix {
+    private static int _getSizeOfArray() {
+        System.out.print("Enter size of array: ");
+
+        return _createScanner().nextInt();
+    }
+
+    private static Scanner _createScanner() {
+        return new Scanner(System.in);
+    }
+
+    private static int _getRandomNumber() {
+        return (int)(0 + Math.random() * 10);
+    }
+
+    public static int[][] CreateRandomMatrix() {
+        int[][] newArray = new int[_getSizeOfArray()][_getSizeOfArray()];
+
+        for (int i = 0; i < newArray.length; i++) {
+            for (int j = 0; j < newArray.length; j++) {
+                newArray[i][j] = _getRandomNumber();
+            }
+        }
+
+        return newArray;
+    }
+
+    public static int[][] CreateRandomMatrix(int n, int m) {
+        int[][] newArray = new int[n][m];
+
+        for (int i = 0; i < newArray.length; i++) {
+            for (int j = 0; j < newArray.length; j++) {
+                newArray[i][j] = _getRandomNumber();
+            }
+        }
+
+        return newArray;
+    }
+
+    public static int[][][] CreateRandomMatrix(int n, int m, int k) {
+        int[][][] newArray = new int[n][m][k];
+
+        for (int i = 0; i < newArray.length; i++)
+            for (int j = 0; j < newArray.length; j++)
+                for (int v = 0; v < newArray.length; v++)
+                    newArray[i][j][v] = _getRandomNumber();
+
+        return newArray;
+    }
+
+    public static void PrintMatrix(int[][][] values) {
+        for (int i = 0; i < values.length; i++) {
+            for (int j = 0; j < values.length; j++) {
+                for (int v = 0; v < values.length; v++) {
+                    System.out.print("arr[" + i + "][" + j + "][" + v + "] = " + values[i][j][v] + "\t");
+                }
+
+                System.out.println();
+            }
+        }
+    }
+}
