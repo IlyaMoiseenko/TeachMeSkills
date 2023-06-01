e3b2c8a6abcd6aafbba32c7530a84333e35fb4b3
IlyaMoiseenko <ilya.moiseenko.00@yandex.by>
2023-06-01 20:27:48 +0300

    Task #1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // TASK #1
+//        int[][][] values = WorkWithMatrix.CreateRandomMatrix(5, 5, 5);
+//        WorkWithMatrix.PrintMatrix(values);
+//        _increaseTheValues(values);
+//        WorkWithMatrix.PrintMatrix(values);
+    }
 
+    private static void _increaseTheValues(int[][][] values) {
+        System.out.print("Value: ");
+        int guessValue = _createScanner().nextInt();
+
+        for (int i = 0; i < values.length; i++)
+            for (int j = 0; j < values.length; j++)
+                for (int v = 0; v < values.length; v++)
+                    values[i][j][v] += guessValue;
+    }
+
+    private static Scanner _createScanner() {
+        return new Scanner(System.in);
     }
 }
