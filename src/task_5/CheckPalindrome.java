cbca5134bee6b611ab86b8096b841a2f9a1243df
IlyaMoiseenko <ilya.moiseenko.00@yandex.by>
2023-06-11 13:13:50 +0300

    fix Added an index check in task #5

--- src/task_5/CheckPalindrome.java
+++ src/task_5/CheckPalindrome.java
@@ -3,8 +3,16 @@ package task_5;
 public class CheckPalindrome {
     public static boolean IsPalindrome(String sentence, int wordIndex) {
         String[] words = sentence.split(" ");
-        String reverseWord = new StringBuilder(words[wordIndex]).reverse().toString();
 
-        return words[wordIndex].equals(reverseWord);
+        if (_checkWordIndex(wordIndex, words)) {
+            String reverseWord = new StringBuilder(words[wordIndex]).reverse().toString();
+            return words[wordIndex].equals(reverseWord);
+        }
+
+        return false;
+    }
+
+    private static boolean _checkWordIndex(int index, String[] words) {
+        return index > words.length || index < words.length;
     }
 }
