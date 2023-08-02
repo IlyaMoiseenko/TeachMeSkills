625c3e01b4e9e48e753e5964908c4f8e259758c2
Ilya Moiseenko <ilya.moiseenko.00@yandex.by>
2023-08-02 19:08:31 +0300

    task #1

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Application application = new Application();
         System.out.println(application.getNums());
+        application.start();
     }
 }
--- src/task_1/Application.java
+++ src/task_1/Application.java
@@ -16,6 +16,58 @@ public class Application {
         this.nums = createRandomNumsList();
     }
 
+    public void start() {
+        deleteDuplicates();
+        evenNumbers();
+        multiply();
+        sortedNumsLimitFour();
+        countOfStream();
+        average();
+    }
+
+    private void deleteDuplicates() {
+        System.out.println("Nums without duplicates");
+
+        List<Integer> numsWithoutDuplicates = nums.stream().distinct().toList();
+        System.out.println(numsWithoutDuplicates);
+    }
+
+    private void evenNumbers() {
+        System.out.println("Even numbers in the range from 7 to 17");
+
+        List<Integer> evenNumbers = nums.stream().filter(num -> num >= 7 && num <= 17 && isEven(num)).toList();
+        System.out.println(evenNumbers);
+    }
+
+    private void multiply() {
+        System.out.println("Multiply nums");
+
+        List<Integer> multiplyNums = nums.stream().map(num -> num * 2).toList();
+        System.out.println(multiplyNums);
+    }
+
+    private void sortedNumsLimitFour() {
+        System.out.println("Sorted nums with limit 4");
+
+        List<Integer> sortedNums = nums.stream().sorted().limit(4).toList();
+        System.out.println(sortedNums);
+    }
+
+    private void countOfStream() {
+        System.out.println("Count of stream");
+
+        System.out.println(nums.stream().count());
+    }
+
+    private void average() {
+        System.out.println("Average");
+        System.out.println(nums.stream().mapToInt(num -> num).average());
+    }
+
+    private boolean isEven(Integer num) {
+        return num % 2 == 0;
+    }
+
     private List<Integer> createRandomNumsList() {
         List<Integer> nums = new ArrayList<>();
 
